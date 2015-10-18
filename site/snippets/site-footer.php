66b8ee4b405904b1e1e5d56c14dec36920ec64fd
Adis Corovic <adis@live.nl>
2015-10-18 18:32:37 +0200

    Fix footer typo

--- site/snippets/site-footer.php
+++ site/snippets/site-footer.php
@@ -1,4 +1,4 @@
 <footer class="site-footer clearfix">
     <section class="copyright"><a href="<?php $site->url() ?>"><?php echo strtolower($site->title()) ?></a> © <?php echo date("Y", time()); ?></section>
-    <section class="poweredby">Proudly written generated with <a href="http://getkirby.com/">Kirby</a></section>
+    <section class="poweredby">Proudly written with <a href="http://getkirby.com/">Kirby</a></section>
 </footer>
